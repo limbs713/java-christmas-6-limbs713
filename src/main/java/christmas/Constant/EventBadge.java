bf595372894a6d2d2da92d3d3a5c7313ee0289cf
hyeongjun im <limbs713@naver.com>
2023-11-11 01:49:45 +0900

    :sparkles: EventBadge에 name 인스턴스 필드 추가
    출력 메시지를 enum value가 가지고 있도록 수정

--- src/main/java/christmas/Constant/EventBadge.java
+++ src/main/java/christmas/Constant/EventBadge.java
@@ -4,16 +4,22 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public enum EventBadge {
-    NO_BADGE(0), STAR(5000), TREE(10000), SANTA(20000);
+    NO_BADGE(0,"없음"), STAR(5000,"별"), TREE(10000,"트리"), SANTA(20000,"산타");
 
     private final int condition;
+    private final String name;
 
-    EventBadge(int condition) {
+    EventBadge(int condition , String name) {
         this.condition = condition;
+        this.name = name;
     }
 
     private int getCondition() {
         return condition;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public static EventBadge getBadge(int totalDiscountPrice) {
