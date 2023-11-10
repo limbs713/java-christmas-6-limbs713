4b60ac0dd8d86caa959bbe3dde25b92b7d0494fd
hyeongjun im <limbs713@naver.com>
2023-11-11 01:43:25 +0900

    :sparkles: Reservation 클래스 추가
    예약 정보를 관리하는 객체

---
+++ src/main/java/christmas/domain/reserve/Reservation.java
@@ -0,0 +1,45 @@
+package christmas.domain.reserve;
+
+import christmas.Constant.EventBadge;
+import christmas.domain.order.MenuItem;
+import java.util.List;
+import java.util.Map;
+
+public class Reservation {
+    private final Receipt receipt;
+    private final DiscountBenefits discountBenefits;
+
+    public Reservation(List<MenuItem> menuItems, int reservationDate) {
+        this.receipt = Receipt.create(menuItems);
+        this.discountBenefits = DiscountBenefits.of(this.receipt, reservationDate);
+    }
+
+    public Map<String, Long> getOrderReceipt() {
+        return receipt.getOrderReceipt();
+    }
+
+    public int getTotalOrderPrice() {
+        return receipt.getTotalPrice();
+    }
+
+    public boolean isBonusBenefitExist() {
+        return discountBenefits.isBonusExist();
+    }
+
+    public Map<String, Integer> getBenefitSpecification() {
+        return discountBenefits.getBenefitSpecification();
+    }
+
+    public int getTotalBenefitPrice() {
+        return discountBenefits.getTotalBenefitPrice();
+    }
+
+    public int getTotalDiscountPrice() {
+        return discountBenefits.getTotalDiscountPrice();
+    }
+
+    public EventBadge getEventBadge() {
+        int totalBenefitPrice = discountBenefits.getTotalBenefitPrice();
+        return EventBadge.getBadge(totalBenefitPrice);
+    }
+}
