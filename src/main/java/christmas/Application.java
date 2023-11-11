f81ecd06ecaabf41ec51df09cc0defc670037856
hyeongjun im <limbs713@naver.com>
2023-11-11 23:40:10 +0900

    :sparkles: Application에 Controller 호출 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,15 @@
 package christmas;
 
+import christmas.controller.OrderController;
+import christmas.controller.ReserveController;
+import christmas.dto.OrderDto;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OrderController orderController = new OrderController();
+        ReserveController reserveController = new ReserveController();
+
+        OrderDto orderDto = orderController.order();
+        reserveController.reserve(orderDto);
     }
 }
