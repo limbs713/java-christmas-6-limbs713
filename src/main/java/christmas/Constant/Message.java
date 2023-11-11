7e4d59634faa3266ab22cc6d058fb964b004c83d
hyeongjun im <limbs713@naver.com>
2023-11-12 03:34:03 +0900

    :sparkles: 예약 정보를 출력하는 기능 추가
    OutputView : ReserveDto에서 예약 정보를 출력한다.
    
    Message : 같이 출력될 메시지와 출력 양식을 저장한다.

--- src/main/java/christmas/Constant/Message.java
+++ src/main/java/christmas/Constant/Message.java
@@ -4,16 +4,22 @@ public enum Message {
     WELLCOME_TITLE_MESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
     INPUT_RESERVATION_DATE_TITLE_MESSAGE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
     INPUT_ORDER_TITLE_MESSAGE("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
+    INPUT_RESERVATION_DATE_ERROR_MESSAGE("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    INPUT_ORDER_MENU_ERROR_MESSAGE("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
     BENEFIT_TITLE_MESSAGE("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
-    OUTPUT_ORDER_MENU_TITLE_MESSAGE("<주문 메뉴>"),
+    OUTPUT_ORDER_MENU_TITLE_MESSAGE("\n<주문 메뉴>"),
     OUTPUT_ORDER_MENU_MESSAGE("%s %d개"),
-    OUTPUT_TOTAL_PRICE_BEFORE_DISCOUNT_TITLE_MESSAGE("<할인 전 총주문 금액>"),
-    OUTPUT_PRICE_WITH_COMMA_MESSAGE("%,d"),
-    OUTPUT_BONUS_BENEFIT_MENU_MESSAGE("<증정 메뉴>"),
-    OUTPUT_TOTAL_BENEFIT_TITLE_MESSAGE("<혜택 내역>"),
-    OUTPUT_TOTAL_BENEFIT_PRICE_TITLE_MESSAGE("<총혜택 금액>"),
-    OUTPUT_TOTAL_PRICE_AFTER_DISCOUNT_TITLE_MESSAGE("<할인 후 예상 결제 금액>"),
-    OUTPUT_EVENT_BADGE_TITLE_MESSAGE("<12월 이벤트 배지>");
+    OUTPUT_TOTAL_PRICE_BEFORE_DISCOUNT_TITLE_MESSAGE("\n<할인 전 총주문 금액>"),
+    OUTPUT_TOTAL_PRICE_MESSAGE("%,d"),
+    OUTPUT_BONUS_MENU_TITLE_MESSAGE("\n<증정 메뉴>"),
+    OUTPUT_BONUS_MENU_MESSAGE("샴페인 1개"),
+    OUTPUT_NO_BENEFIT_MESSAGE("없음"),
+    OUTPUT_BENEFIT_MESSAGE("%s: -%,d"),
+    OUTPUT_BENEFIT_TITLE_MESSAGE("\n<혜택 내역>"),
+    OUTPUT_BENEFIT_PRICE_TITLE_MESSAGE("\n<총혜택 금액>"),
+    OUTPUT_BENEFIT_PRICE_MESSAGE("-%,d"),
+    OUTPUT_TOTAL_PRICE_AFTER_DISCOUNT_TITLE_MESSAGE("\n<할인 후 예상 결제 금액>"),
+    OUTPUT_EVENT_BADGE_TITLE_MESSAGE("\n<12월 이벤트 배지>");
 
     private final String message;
 
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,8 +1,79 @@
 package christmas.view;
 
+import static christmas.Constant.Message.*;
+
+import christmas.dto.ReserveDto;
+import java.util.Map;
+
 public class OutputView {
 
     public static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void printReservation(ReserveDto reserveDto) {
+        printMessage(BENEFIT_TITLE_MESSAGE.getMessage());
+
+        printOrderMenu(reserveDto.getOrderMenu());
+        printOrderPriceBeforeDiscount(reserveDto.getTotalPriceBeforeDiscount());
+        printBonusMenu(reserveDto.getBonusMenu());
+        printBenefits(reserveDto.getBenefits());
+        printBenefitPrice(reserveDto.getTotalBenefitPrice());
+        printOrderPriceAfterDiscount(reserveDto.getTotalPriceAfterDiscount());
+        printEventBadge(reserveDto.getEventBadge());
+    }
+
+    private static void printEventBadge(String eventBadge) {
+        printMessage(OUTPUT_EVENT_BADGE_TITLE_MESSAGE.getMessage());
+
+        printMessage(eventBadge);
+    }
+
+    private static void printOrderPriceAfterDiscount(int totalPriceAfterDiscount) {
+        printMessage(OUTPUT_TOTAL_PRICE_AFTER_DISCOUNT_TITLE_MESSAGE.getMessage());
+
+        printMessage(String.format(OUTPUT_TOTAL_PRICE_MESSAGE.getMessage(), totalPriceAfterDiscount));
+    }
+
+    private static void printBenefitPrice(int totalBenefitPrice) {
+        printMessage(OUTPUT_BENEFIT_PRICE_TITLE_MESSAGE.getMessage());
+        printMessage(String.format(OUTPUT_BENEFIT_PRICE_MESSAGE.getMessage(),totalBenefitPrice));
+    }
+
+    private static void printBenefits(Map<String, Integer> benefits) {
+        printMessage(OUTPUT_BENEFIT_TITLE_MESSAGE.getMessage());
+
+        if(benefits.isEmpty()){
+            printMessage(OUTPUT_NO_BENEFIT_MESSAGE.getMessage());
+        }
+
+        if(!benefits.isEmpty()){
+            benefits.forEach((key, value) ->
+                    printMessage(String.format(OUTPUT_BENEFIT_MESSAGE.getMessage(), key, value)));
+        }
+    }
+
+    private static void printBonusMenu(boolean isBonusMenuExist) {
+        printMessage(OUTPUT_BONUS_MENU_TITLE_MESSAGE.getMessage());
+
+        if(isBonusMenuExist) {
+            printMessage(OUTPUT_BONUS_MENU_MESSAGE.getMessage());
+        }
+
+        if(!isBonusMenuExist) {
+            printMessage(OUTPUT_NO_BENEFIT_MESSAGE.getMessage());
+        }
+    }
+
+    private static void printOrderPriceBeforeDiscount(int totalPriceBeforeDiscount) {
+        printMessage(OUTPUT_TOTAL_PRICE_BEFORE_DISCOUNT_TITLE_MESSAGE.getMessage());
+
+        printMessage(String.format(OUTPUT_TOTAL_PRICE_MESSAGE.getMessage(), totalPriceBeforeDiscount));
+    }
+
+    private static void printOrderMenu(Map<String, Integer> orderMenu) {
+        printMessage(OUTPUT_ORDER_MENU_TITLE_MESSAGE.getMessage());
+
+        orderMenu.forEach((key, value) -> printMessage(String.format(OUTPUT_ORDER_MENU_MESSAGE.getMessage(),key,value)));
     }
 }
