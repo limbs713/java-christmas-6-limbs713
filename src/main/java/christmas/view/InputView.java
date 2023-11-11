a0eab62ef41e9a11688c48fc3b729e8ce61aecce
hyeongjun im <limbs713@naver.com>
2023-11-11 23:37:55 +0900

    :sparkles: InputView 추가
    입력값을 받아 반환하는 기능 추가

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,11 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String input() {
+        return Console.readLine();
+    }
+
+}
