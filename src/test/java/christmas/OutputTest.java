24a5017b7e8e66d80ea9da9802fad1da05e821da
hyeongjun im <limbs713@naver.com>
2023-11-14 00:48:59 +0900

    :recycle: ApplicationTest.java 이름 수정

--- src/test/java/christmas/OutputTest.java
+++ src/test/java/christmas/OutputTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-class ApplicationTest extends NsTest {
+class OutputTest extends NsTest {
     private static final String LINE_SEPARATOR = System.lineSeparator();
 
     @Test
